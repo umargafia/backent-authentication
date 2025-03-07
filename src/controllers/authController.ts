773c9c858d4169ffee706e436bc4dcc04efa480e
umar musa <umargafia@gmail.com>
2025-03-07 17:59:14 +0100

    Refactor error handling with catchAsync utility

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,79 +2,69 @@ import { Request, Response, NextFunction } from 'express';
 import { User } from '../models/User';
 import { AuthService } from '../services/authService';
 import { AppError } from '../utils/AppError';
+import { catchAsync } from '../utils/catchAsync';
 import crypto from 'crypto';
+import { env } from '../config/env';
 
-export const signup = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-    });
+export const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-    AuthService.createSendToken(newUser, 201, res);
-  } catch (error) {
-    next(error);
+  AuthService.createSendToken(newUser, 201, res);
+});
+
+export const login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
   }
-};
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { email, password } = req.body;
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
 
-    // 1) Check if email and password exist
-    if (!email || !password) {
-      return next(new AppError('Please provide email and password!', 400));
-    }
-
-    // 2) Check if user exists && password is correct
-    const user = await User.findOne({ email }).select('+password');
-
-    if (!user || !(await user.correctPassword(password, user.password))) {
-      return next(new AppError('Incorrect email or password', 401));
-    }
-
-    // 3) If everything ok, send token to client
-    AuthService.createSendToken(user, 200, res);
-  } catch (error) {
-    next(error);
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
   }
-};
 
-export const protect = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // 1) Getting token and check if it exists
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-      token = req.headers.authorization.split(' ')[1];
-    }
+  // 3) If everything ok, send token to client
+  AuthService.createSendToken(user, 200, res);
+});
 
-    if (!token) {
-      return next(new AppError('You are not logged in! Please log in to get access.', 401));
-    }
-
-    // 2) Verification token
-    const decoded = AuthService.verifyToken(token);
-
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) {
-      return next(new AppError('The user belonging to this token no longer exists.', 401));
-    }
-
-    // 4) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
-      return next(new AppError('User recently changed password! Please log in again.', 401));
-    }
-
-    // GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser;
-    next();
-  } catch (error) {
-    next(error);
+export const protect = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  // 1) Getting token and check if it exists
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
   }
-};
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  // 2) Verification token
+  const decoded = AuthService.verifyToken(token);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(new AppError('The user belonging to this token no longer exists.', 401));
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(new AppError('User recently changed password! Please log in again.', 401));
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+});
 
 export const restrictTo = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -86,8 +76,8 @@ export const restrictTo = (...roles: string[]) => {
   };
 };
 
-export const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const forgotPassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     // 1) Get user based on POSTed email
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
@@ -123,42 +113,36 @@ export const forgotPassword = async (req: Request, res: Response, next: NextFunc
 
       return next(new AppError('There was an error sending the email. Try again later!', 500));
     }
-  } catch (error) {
-    next(error);
   }
-};
+);
 
-export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // 1) Get user based on the token
-    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+export const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
 
-    const user = await User.findOne({
-      passwordResetToken: hashedToken,
-      passwordResetExpires: { $gt: Date.now() },
-    });
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
 
-    // 2) If token has not expired, and there is user, set the new password
-    if (!user) {
-      return next(new AppError('Token is invalid or has expired', 400));
-    }
-
-    // 3) Update password
-    user.password = req.body.password;
-    user.passwordConfirm = req.body.passwordConfirm;
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    await user.save();
-
-    // 4) Log the user in, send JWT
-    AuthService.createSendToken(user, 200, res);
-  } catch (error) {
-    next(error);
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
   }
-};
 
-export const updatePassword = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+  // 3) Update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 4) Log the user in, send JWT
+  AuthService.createSendToken(user, 200, res);
+});
+
+export const updatePassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     // 1) Get user from collection
     const user = await User.findById(req.user.id).select('+password');
     if (!user) {
@@ -177,7 +161,5 @@ export const updatePassword = async (req: Request, res: Response, next: NextFunc
 
     // 4) Log user in, send JWT
     AuthService.createSendToken(user, 200, res);
-  } catch (error) {
-    next(error);
   }
-};
+);
